4cc8c660f684ec25b9dda9a4d5034b9685898e2c
Fhchcjcjcjcjvkkvk <info@infopeklo.cz>
2024-12-07 08:54:45 +0100

    Update airhunter.c

--- airhunter.c
+++ airhunter.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAX_CMD_LEN 256
 #define MAX_OUTPUT_LEN 1024
@@ -10,7 +11,6 @@ void run_tshark(char *cmd, char *output) {
     FILE *fp;
     char buffer[MAX_OUTPUT_LEN];
 
-    // Open the command for reading
     fp = _popen(cmd, "r");
     if (fp == NULL) {
         perror("Error running tshark");
@@ -28,17 +28,17 @@ void run_tshark(char *cmd, char *output) {
 // Function to parse the tshark output and display network information
 void parse_tshark_output(char *output) {
     char *line = strtok(output, "\n");
+    int beacon_count = 0;
 
     while (line != NULL) {
-        char bssid[18], essid[33], beacon_count[10], live_time[20];
-
-        // Search for a line containing the BSSID, ESSID, and other information
-        if (sscanf(line, "BSSID: %17s ESSID: \"%32[^\"]\" Beacon count: %9s Live time: %19s", 
-                    bssid, essid, beacon_count, live_time) == 4) {
+        char bssid[18], essid[33];
+        
+        // Search for a line containing the BSSID and ESSID
+        if (sscanf(line, "BSSID: %17s ESSID: \"%32[^\"]\"", bssid, essid) == 2) {
+            beacon_count++;  // Increment the beacon count for each packet
             printf("BSSID: %s\n", bssid);
             printf("ESSID: %s\n", essid);
-            printf("Beacon Count: %s\n", beacon_count);
-            printf("Live Time: %s\n", live_time);
+            printf("Beacon Count: %d\n", beacon_count);
             printf("-------------------------\n");
         }
 
@@ -50,9 +50,9 @@ int main() {
     char cmd[MAX_CMD_LEN];
     char output[MAX_OUTPUT_LEN] = "";
 
-    // Command to run tshark and capture beacon frames on Windows
+    // Command to run tshark and capture beacon frames
     // Ensure to replace "Wi-Fi" with the name of your network interface
-    snprintf(cmd, sizeof(cmd), "tshark -i Wi-Fi -T fields -e wlan.bssid -e wlan.ssid -e wlan_beacon.beacon_count -e frame.time -f \"type mgt subtype beacon\"");
+    snprintf(cmd, sizeof(cmd), "tshark -i Wi-Fi -T fields -e wlan.bssid -e wlan.ssid -f \"type mgt subtype beacon\"");
 
     printf("Scanning for nearby networks...\n");
 
