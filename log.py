66882fe808237a298e91c11a37fdebca43c9532e
Fhchcjcjcjcjvkkvk <info@infopeklo.cz>
2024-12-27 15:28:59 +0100

    Update log.py

--- log.py
+++ log.py
@@ -26,12 +26,15 @@ def brute_force_attack(username, password_list, url, success_url, port, random_a
         except requests.exceptions.RequestException as e:
             print(f"Error: {e}")
             continue
+    else:
+        # If the loop completes without finding the key
+        print("KEY NOT FOUND")
 
 def main():
     parser = argparse.ArgumentParser(description="Mephisto Brute Force Tool")
     parser.add_argument('-l', '--username', type=str, required=True, help="Target username")
     parser.add_argument('-P', '--password', type=str, required=True, help="Path to password list file")
-    parser.add_argument('url', type=str, required=True, help="Target login URL")
+    parser.add_argument('url', type=str, help="Target login URL")
     parser.add_argument('--redirect', type=str, required=True, help="Success URL (redirect to this URL when successful)")
     parser.add_argument('--port', type=int, required=True, help="Port of the target server")
     parser.add_argument('--random-agent', action='store_true', help="Use random User-Agent headers")
