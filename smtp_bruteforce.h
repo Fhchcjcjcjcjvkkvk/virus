49048024a74dec6487375b66459c59773217124a
Fhchcjcjcjcjvkkvk <info@infopeklo.cz>
2024-12-28 14:20:34 +0100

    Update and rename smtp_bruteforce.cpp to smtp_bruteforce.h

---
+++ smtp_bruteforce.h
@@ -0,0 +1,13 @@
+#ifndef SMTP_BRUTEFORCE_H
+#define SMTP_BRUTEFORCE_H
+
+#include <string>
+
+bool initWinsock();
+SOCKET connectToSMTPServer(const std::string &hostname, int port);
+bool sendData(SOCKET sock, const std::string &data);
+std::string receiveData(SOCKET sock);
+bool bruteForceSMTP(const std::string &username, const std::string &password, const std::string &hostname, int port);
+std::string base64Encode(const std::string &in);
+
+#endif
