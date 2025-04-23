52121470a494dd6d87728f5e0a6a4565a5c8ed47
Fhchcjcjcjcjvkkvk <info@infopeklo.cz>
2025-04-23 11:11:27 +0200

    Update crypto_utils.py

--- crypto_utils.py
+++ crypto_utils.py
@@ -3,7 +3,7 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
 KEY = b'54fdd053beb8dfd181d89f7f0d2dc09118f02892d559f438bc672a569fba7c24'  # 32 bytes
-IV = b'initialvector123'  # 16 bytes
+IV = b'74c8a9f3a3a346318de451a1b67ca298'  # 16 bytes
 
 def encrypt_and_compress(data: bytes) -> bytes:
     compressed = zlib.compress(data)
